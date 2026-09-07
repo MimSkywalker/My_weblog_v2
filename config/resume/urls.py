3d95a89ae5c85cdbb11780dfa58e03e7aade6fd4
Mohammad <mdmdnezhad@gmail.com>
2026-09-07 18:57:05 +0330

    Update resume urls

--- config/resume/urls.py
+++ config/resume/urls.py
@@ -3,14 +3,7 @@ from . import views
 app_name = 'resume'
 urlpatterns = [
     path("managment/", views.PortfolioDashboardView.as_view(), name='resume_manager'),
-    path("", views.test.as_view(), name='resume_manager'),
-    path("1", views.test1.as_view(), name='resume_manager'),
-    path("2", views.test2.as_view(), name='resume_manager'),
-    path("3", views.test3.as_view(), name='resume_manager'),
-
-
-
-
+    path("", views.test.as_view(), name='resume'),
 
 
 ]
