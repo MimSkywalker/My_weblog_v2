bf93821003c3ff190048e889f34d63791f9dc4a2
Mohammad <mdmdnezhad@gmail.com>
2026-09-07 20:02:58 +0330

    Update Project's url's name

--- config/project/urls.py
+++ config/project/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 app_name = 'project'
 urlpatterns = [
-    path("", views.ProjectView.as_view(), name='resume_manager'),
+    path("", views.ProjectView.as_view(), name='project'),
 
 ]
