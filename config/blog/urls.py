409d01ab47ea0cedcd21f01a715a42266c5c610f
Mohammad <mdmdnezhad@gmail.com>
2026-09-08 11:03:20 +0330

    Add test-view for blog

--- config/blog/urls.py
+++ config/blog/urls.py
@@ -3,5 +3,7 @@ from . import views
 app_name = 'blog'
 urlpatterns = [
     path("", views.BlogView.as_view(), name='blog'),
+    path("1", views.BlogDetailView.as_view(), name='blog-detail'),
+
 
 ]
--- config/blog/views.py
+++ config/blog/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
-
 class BlogView(TemplateView):
-    template_name ='blog/blog.html'
+    template_name = 'blog/blog.html'
+
+
+class BlogDetailView(TemplateView):
+    template_name = 'blog/blog-detail.html'
